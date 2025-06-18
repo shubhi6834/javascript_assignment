a0536b5c3054d14333535f628b8f371ba7ca52ea
shubhi6834 <ashubhi731@gmail.com>
2025-06-18 11:46:04 +0530

    Create multilication-division.js

---
+++ multilication-division.js
@@ -0,0 +1,7 @@
+function calculate() {
+  const num1 = parseFloat(prompt("Enter the first number:"));
+  const num2 = parseFloat(prompt("Enter the second number:"));
+  console.log("Multiplication: " + (num1 * num2));
+  console.log("Division: " + (num1 / num2));
+}
+calculate();
