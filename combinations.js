b643940f5ec8a2142d85767363cac04240290a4a
shubhi6834 <ashubhi731@gmail.com>
2025-06-19 16:57:04 +0530

    Update combinations.js

--- combinations.js
+++ combinations.js
@@ -1,12 +1,16 @@
 function stringCombinations(str) {
   const result = [];
+  const ans = [];
   for (let i = 0; i < str.length; i++) {
     let combination = '';
     for (let j = i; j < str.length; j++) {
       combination += str[j];
-      result.push(combination);
+      if(!result[combination]){
+       result[combination]=1;
+       ans.push(combination);
+      }
     }
   }
-  return result;
+  return ans;
 }
 console.log(stringCombinations("dog")); 
