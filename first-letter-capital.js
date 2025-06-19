34cd39bf5331c488c47cfa196d72d21b4e9b8c75
shubhi6834 <ashubhi731@gmail.com>
2025-06-19 10:45:49 +0530

    Create first-letter-capital.js

---
+++ first-letter-capital.js
@@ -0,0 +1,9 @@
+function capitalizeWords(str) {
+  return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+console.log(capitalizeWords("the quick brown fox"));  
+
