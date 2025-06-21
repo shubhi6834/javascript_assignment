0fd7338aaac8c0b13deac4644ae2db592ab76d34
shubhi6834 <ashubhi731@gmail.com>
2025-06-21 11:15:13 +0530

    Create 12.js

---
+++ Javascript Array/12.js
@@ -0,0 +1,14 @@
+
+var a = [1, 2, 5, 1];
+var sum = 0 ;
+var product = 1 ;
+for(let i = 0 ; i < a.length ; i++){
+    sum += parseInt(a[i]);
+    product *= parseInt(a[i]);
+}
+if(!Number.isNaN(sum) && !Number.isNaN(product)){
+    console.log("The sum is : " + sum);
+    console.log("The product is : " + product);
+}
+else
+console.log("Enter an array with numeric digits");
