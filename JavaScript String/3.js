9190100f8b8291b256f17301647edaf76ac3a279
shubhi6834 <ashubhi731@gmail.com>
2025-06-21 11:59:52 +0530

    Create 3.js

---
+++ JavaScript String/3.js
@@ -0,0 +1,5 @@
+function string_to_array(str) { 
+return str.split(' '); 
+} 
+console.log(string_to_array("Robin Singh")); 
+Test Data : console.log(string_to_array("Robin Singh")); ["Robin", "Singh"]
