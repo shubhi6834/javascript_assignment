24d4d743c6609ec82644e175f9c70d180592d5d8
shubhi6834 <ashubhi731@gmail.com>
2025-06-21 11:47:24 +0530

    Create 37.js

---
+++ Javascript Array/37.js
@@ -0,0 +1,6 @@
+function array_filled_string(count, value) {  
+         return new Array(count).fill(value); 
+ }  
+ 
+console.log(array_filled_string(3, 'default value'));  
+console.log(array_filled_string(4, 'password'));  
